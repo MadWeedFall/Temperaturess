78aad337abca78cc1c4229754b95aa5c7c94407a
MadWeedFall <lixuhit@gmail.com>
2012-10-17 10:02:05 +0800

    first commit

---
+++ WDN/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WDN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.weiboBtn = new System.Windows.Forms.Button();
+            this.statTxt = new System.Windows.Forms.Label();
+            this.arduinoSerial = new System.IO.Ports.SerialPort(this.components);
+            this.WeiboUpTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(77, 252);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(132, 36);
+            this.readBtn.TabIndex = 0;
+            this.readBtn.Text = "开始读取串口数据";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // weiboBtn
+            // 
+            this.weiboBtn.Location = new System.Drawing.Point(294, 252);
+            this.weiboBtn.Name = "weiboBtn";
+            this.weiboBtn.Size = new System.Drawing.Size(140, 36);
+            this.weiboBtn.TabIndex = 1;
+            this.weiboBtn.Text = "将数据发送至微薄";
+            this.weiboBtn.UseVisualStyleBackColor = true;
+            this.weiboBtn.Click += new System.EventHandler(this.weiboBtn_Click);
+            // 
+            // statTxt
+            // 
+            this.statTxt.AutoSize = true;
+            this.statTxt.Location = new System.Drawing.Point(106, 60);
+            this.statTxt.Name = "statTxt";
+            this.statTxt.Size = new System.Drawing.Size(77, 12);
+            this.statTxt.TabIndex = 2;
+            this.statTxt.Text = "系统状态信息";
+            // 
+            // arduinoSerial
+            // 
+            this.arduinoSerial.PortName = "COM4";
+            this.arduinoSerial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.arduinoSerial_DataReceived);
+            // 
+            // WeiboUpTimer
+            // 
+            this.WeiboUpTimer.Interval = 600000;
+            this.WeiboUpTimer.Tick += new System.EventHandler(this.WeiboUpTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 325);
+            this.Controls.Add(this.statTxt);
+            this.Controls.Add(this.weiboBtn);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Form1";
+            this.Text = "交大1119温度娘v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.Button weiboBtn;
+        private System.Windows.Forms.Label statTxt;
+        private System.IO.Ports.SerialPort arduinoSerial;
+        private System.Windows.Forms.Timer WeiboUpTimer;
+    }
+}
+
---
+++ WDN/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using NetDimension.Weibo;
+
+namespace WDN
+{
+    public partial class Form1 : Form
+    {
+        string appKey;
+        string appSecret;
+        string accessToken;
+        Client sina;
+        bool serialReadFlag;
+        string temperature;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            appKey = Properties.Settings.Default.AppKey;
+            appSecret = Properties.Settings.Default.AppSecret;
+            accessToken = Properties.Settings.Default.AcessToken;
+            serialReadFlag = false;
+
+            OAuth oauth = null;
+            sina = null;
+            if (string.IsNullOrEmpty(accessToken))//如果本地未保存accessToken，这里没有做过期判断2012-10-5
+            {
+                oauth = Authorize();
+                if (!string.IsNullOrEmpty(oauth.AccessToken))
+                {
+                    Properties.Settings.Default.AcessToken = oauth.AccessToken;
+                    Properties.Settings.Default.Save();
+                    statTxt.Text = "授权成功，保存accesstoken";
+                    sina = new Client(oauth);
+                }
+                else
+                {
+                    statTxt.Text = "授权失败，未获取accesstoken";
+                }
+            }
+            else
+            {
+                oauth = new OAuth(appKey, appSecret, accessToken, "");//最后一个参数是refreshtoken这个是面向高级用户对的
+                TokenResult tokenResult = oauth.VerifierAccessToken();
+                if (tokenResult == TokenResult.Success)
+                {
+                    statTxt.Text = "验证本地accesstoken有效";
+                    sina = new Client(oauth);
+                }
+                else
+                {                   
+                    Properties.Settings.Default.AcessToken = string.Empty;
+                    Properties.Settings.Default.Save();
+                    statTxt.Text = "验证本地accesstoken无效，尝试重新授权";
+                    oauth = Authorize();
+                    if (!string.IsNullOrEmpty(oauth.AccessToken))
+                    {
+                        Properties.Settings.Default.AcessToken = oauth.AccessToken;
+                        Properties.Settings.Default.Save();
+                        statTxt.Text = "授权成功，重新保存accesstoken";
+                        sina = new Client(oauth);
+                    }
+                    else
+                    {
+                        statTxt.Text = "授权失败，未获取accesstoken";
+                    }
+                }
+            }
+        }
+
+        static OAuth Authorize()
+        {
+            OAuth oauth = null;
+            oauth = new OAuth(Properties.Settings.Default.AppKey, Properties.Settings.Default.AppSecret, Properties.Settings.Default.CallBackUrl);
+            while (!ClientLogin(oauth))
+            {
+                //尝试登陆直到登上为止
+            }
+            return oauth;
+        }
+
+        private static bool ClientLogin(OAuth o)
+        {
+            string username="onepiecelx@sina.com";
+            string password="marksman";
+
+            return o.ClientLogin(username, password);
+        }
+
+        private void weiboBtn_Click(object sender, EventArgs e)
+        {
+            if (sina != null)
+            {
+                try
+                {
+                    sina.API.Statuses.Update("当前寝室里的温度："+temperature+"摄氏度", 0, 0, null);
+                    statTxt.Text = "成功发布一条新微薄"+temperature;
+                    WeiboUpTimer.Enabled = true;
+                    WeiboUpTimer.Start();
+                }
+                catch (WeiboException ex)
+                {
+                    statTxt.Text = "发布微薄失败，原因" + ex.Message;
+                }
+            }
+
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            serialReadFlag = !serialReadFlag;
+            if (serialReadFlag)
+            {
+                arduinoSerial.Open();
+                readBtn.Text = "停止获取串口数据";
+            }
+            else
+            {
+                arduinoSerial.Close();
+                readBtn.Text = "开始获取串口数据";
+            }
+        }
+
+        private void arduinoSerial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            
+            SerialPort serial = (SerialPort)sender;
+            temperature = serial.ReadLine();
+            temperature = temperature.Replace("\n", "");
+            
+        }
+
+        private void WeiboUpTimer_Tick(object sender, EventArgs e)
+        {
+            if (sina != null)
+            {
+                try
+                {
+                    sina.API.Statuses.Update("当前寝室里的温度：" + temperature + "摄氏度", 0, 0, null);
+                    statTxt.Text = "成功发布一条新微薄"+temperature;
+                    
+                }
+                catch (WeiboException ex)
+                {
+                    statTxt.Text = "发布微薄失败，原因" + ex.Message;
+                }
+            }
+        }
+    }
+}
